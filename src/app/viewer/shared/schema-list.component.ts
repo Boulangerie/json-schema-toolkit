457b1c347dc60dd2bc7de6af41d59d39d0c95781
julien-vidal <julien.vidal@teads.tv>
2016-09-02 18:17:04 +0200

    First version of searchBox

--- src/app/viewer/shared/schema-list.component.ts
+++ src/app/viewer/shared/schema-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
 import { SchemaItemComponent } from "./schema-item.component"
-import { V3Parser, ISchema } from "../../shared/schema/parsers/v3.parser"
+import { ISchema } from "../../shared/schema/parsers/v3.parser"
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list'
 
 @Component({
@@ -9,10 +9,21 @@ import { MD_LIST_DIRECTIVES } from '@angular2-material/list'
   templateUrl: 'schema-list.component.html',
   directives: [SchemaItemComponent, MD_LIST_DIRECTIVES],
 })
-export class SchemaListComponent {
+export class SchemaListComponent implements OnChanges {
   @Input() schemas: Array<ISchema>
 
-  public constructor() {
+  searchText: string
+  schemasList: Object[]
 
+  public constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.schemasList = changes['schemas'].currentValue.slice(0);
+  }
+
+  public filterSchemas(){
+    this.schemasList = this.schemas.filter((item) =>{
+      return item.title.includes(this.searchText);
+    });
   }
 }
